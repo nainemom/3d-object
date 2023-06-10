5581616d40aec550d62b638bc0672d42b17e4d8a
Amir Momenian <nainemom@gmail.com>
2023-06-10 23:37:55 +0330

    clean up code

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import * as faceapi from 'face-api.js';
+import { loadTinyFaceDetectorModel } from 'face-api.js'
 
-
-// faceapi.nets.tinyYolov2.loadFromUri('/models');
-
-faceapi.nets.mtcnn.loadFromUri('/models');
-
-Promise.all([
-  // faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  // faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  // faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-]).then(() => {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const loadApp = async () => {
+  await loadTinyFaceDetectorModel('/models');
+  createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   );
-});
+}
+
+loadApp();
 
